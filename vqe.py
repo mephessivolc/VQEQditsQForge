412d06cdeeb1d8783e18aa071e1b5fa6324d7357
Clovis Caface <clovis.caface@gmail.com>
2026-07-17 18:44:21 -0300

    update 07 17 2026 18:44

--- vqe.py
+++ vqe.py
@@ -6,20 +6,14 @@ import hamiltonian as hl
 
 n = 4 # number of qudits
 dim = 5 # qudit dimension
-device = 'cpu' #'cuda' # or cpu if you dont have cuda
-epochs = 10
+device = 'cuda' # or cpu if you dont have cuda
+epochs = 1000
 
 # Define model and optimizer
 model = models.VQE(n=n, dim=dim, device=device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-# # Define Hamiltonian, here in this example we use a random hamiltonian
-# H = torch.randn((dim**n, dim**n), device=device, dtype=torch.complex64)
-# H = 0.5 * (H + H.conj().T)
-
-# true_ground_energy = torch.linalg.eigvalsh(H).min().item()
-
-# Inicializa a biblioteca do seu Hamiltoniano customizado
+# Initializes the library for your custom Hamiltonian.
 hamiltonian_builder = hl.Hamiltonian(dim=dim, n=n, lambda_param=1.0, device=device)
 H = hamiltonian_builder.H
 true_ground_energy = hamiltonian_builder.true_ground_energy
@@ -49,5 +43,3 @@ plt.axhline(y=true_ground_energy, color='r', linestyle='--')
 plt.grid()
 plt.savefig(f"Resultados{n}_{dim}_{epoch}.png")
 plt.show()
-
-
